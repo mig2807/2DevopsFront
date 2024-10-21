63d06ad2dcea8bce066656f4287897518202d32f
mig2807 <miguelasb28@gmail.com>
2024-10-21 14:11:27 -0500

    se agrega el front

---
+++ src/components/TaskList.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const TaskList = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/tasks/')
+      .then(response => {
+        setTasks(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching tasks:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Task List</h1>
+      <ul>
+        {tasks.map(task => (
+          <li key={task.id}>
+            {task.title} - {task.status}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TaskList;
